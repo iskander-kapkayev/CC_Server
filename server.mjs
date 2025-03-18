88e7af9f70f9e6020414a32f5f1e9ff4122738d0
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-03-18 14:35:24 -0400

    added cors to vercel.json and also commented out cors options in server script

--- server.mjs
+++ server.mjs
@@ -22,14 +22,16 @@ const pool = new Pool({
     idleTimeoutMillis: 300
 });
 
+/*
 const corsOptions ={
    origin: '*', 
    credentials: true, //access-control-allow-credentials:true
    optionSuccessStatus: 200,
 }
-
 app.use(cors(corsOptions));
-app.use(express.json());
+*/
+
+app.use(express.json()); // allows for json post requests
 
 /*
 This section is for password encryption handling.
