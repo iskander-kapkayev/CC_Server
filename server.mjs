fceff2b095f5d2bef9e03a830957e56f51ee6597
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-01 15:03:22 -0400

    bug fix on the word added

--- server.mjs
+++ server.mjs
@@ -230,7 +230,7 @@ async function signin(email, password) {
             const timestamp = now.toISOString().slice(0, 19).replace('T', ' ');
             query = 'UPDATE users SET lastlogin = $1 WHERE email = $2';
             await dbclient.query(query, [timestamp, email]);
-            await dbclient.query('COMMIT'); added
+            await dbclient.query('COMMIT'); //added
 
             // now create usertoken
             query = 'SELECT username FROM users WHERE email = $1';
